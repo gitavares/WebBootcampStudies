d447bc0f5a2636e3e5ce239ea7d2bd5e7a291eec
Giselle Tavares <gitavares@hotmail.com>
2018-08-30 14:31:55 -0400

    authorize publisher to edit a campground

--- YelpCamp/v10/routes/campgrounds.js
+++ YelpCamp/v10/routes/campgrounds.js
@@ -54,17 +54,18 @@ router.get("/:id", function (req, res) {
 })
 
 // EDIT campground route
-router.get("/:id/edit", function (req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function (req, res) {
     Campground.findById(req.params.id, function (err, foundCampground) {
         if(err){
             res.redirect("/campgrounds")
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground})
         }
-        res.render("campgrounds/edit", {campground: foundCampground})
-    }) 
+    })
 })
 
 // UPDATE campground route
-router.put("/:id", function (req, res) {
+router.put("/:id", checkCampgroundOwnership, function (req, res) {
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, updatedCampground) {
         if(err){
@@ -75,7 +76,7 @@ router.put("/:id", function (req, res) {
 })
 
 // DESTROY campground route
-router.delete("/:id", function (req, res) {
+router.delete("/:id", checkCampgroundOwnership, function (req, res) {
     Campground.findByIdAndRemove(req.params.id, function (err) {
         if(err){
             res.redirect("/campgrounds") // must be treated, course
@@ -90,6 +91,24 @@ function isLoggedIn(req, res, next){
         return next()
     }
     res.redirect("/login")
+}
+
+function checkCampgroundOwnership(req, res, next) {
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function (err, foundCampground) {
+            if(err){
+                res.redirect("back")
+            }
+
+            if(foundCampground.author.id.equals(req.user._id)){
+                return next()
+            } else {
+                res.redirect("back")
+            }
+        })
+    } else {
+        res.redirect("back")
+    }
 }
 
 module.exports = router
