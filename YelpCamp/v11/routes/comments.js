7969ec72ffe96a62f30d0cf640ae0ed3ffb3db56
Giselle Tavares <gitavares@hotmail.com>
2018-09-01 18:26:09 -0400

    messages and connect-flash

---
+++ YelpCamp/v11/routes/comments.js
@@ -0,0 +1,79 @@
+var express = require("express")
+var router = express.Router({mergeParams: true})
+var Campground = require("../models/campground")
+var Comment = require("../models/comment")
+var middleware = require("../middleware")
+
+
+// NEW ROUTE (Comments)
+router.get("/new", middleware.isLoggedIn, function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if(err){
+            console.log(err)
+        } else {
+            res.render("comments/new", { campground: foundCampground })
+        }
+    })
+})
+
+// CREATE ROUTE (Comments)
+router.post("/", middleware.isLoggedIn, function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if(err){
+            console.log(err)
+            res.redirect("/campgrounds")
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if(err){
+                    console.log(err)
+                } else {
+                    // add username and id to comment
+                    comment.author.username = req.user.username
+                    comment.author.id = req.user._id
+                    // save comment
+                    comment.save()
+                    
+                    foundCampground.comments.push(comment)
+                    foundCampground.save()
+                    res.flash("success", "Successfully added comment")
+                    res.redirect("/campgrounds/" + foundCampground._id)
+                }
+            })
+        }
+    })
+})
+
+// EDIT ROUTE
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if(err){
+            res.redirect("back")
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment})
+        }
+    })
+})
+
+// UPDATE ROUTE
+router.put("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+        if(err){
+            res.redirect("back")
+        }     
+        req.flash("success", "Comment updated")
+        res.redirect("/campgrounds/" + req.params.id)
+    })
+})
+
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if(err){
+            res.redirect("back")
+        }
+        req.flash("success", "Comment deleted")
+        res.redirect("/campgrounds/" + req.params.id)
+    })
+})
+
+module.exports = router
